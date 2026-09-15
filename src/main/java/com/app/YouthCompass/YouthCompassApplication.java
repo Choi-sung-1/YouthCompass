37321444dd92ef39976967c75ab79ea3518c8ffa
ChoiSungWon <spancho0810@naver.com>
2026-09-15 13:56:39 +0900

    init: Start Srping Project + Mybatis setup

---
+++ src/main/java/com/app/YouthCompass/YouthCompassApplication.java
@@ -0,0 +1,13 @@
+package com.app.YouthCompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthCompassApplication.class, args);
+	}
+
+}
